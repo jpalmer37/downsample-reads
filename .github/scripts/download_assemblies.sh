451610553b765cfb7cc43470f39cef444561f9f1
Dan Fornika <dfornika@gmail.com>
2024-03-18 17:38:04 -0700

    Add testing workflow

---
+++ .github/scripts/download_assemblies.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir -p .github/data/assemblies
+
+curl -o .github/data/assemblies/NC_000962.3.fa "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?retmode=text&id=NC_000962.3&db=nucleotide&rettype=fasta"
+curl -o .github/data/assemblies/NC_000913.3.fa "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?retmode=text&id=NC_000913.3&db=nucleotide&rettype=fasta"
